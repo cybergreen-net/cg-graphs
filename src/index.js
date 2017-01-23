2f33efd25b8e63afa1965971e71a7f458e5610ad
zelima <ir.mchedlishvili@gmail.com>
2017-01-23 14:04:21 +0400

    [data][s]: global variables for data moved out index.js to html file

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
+/* global graphData */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-const fixtures = [
-  {
-    url: './fixtures/dns.json',
-    name: 'Open Recursive DNS'
-  },
-  {
-    url: './fixtures/ntp.json',
-    name: 'Open NTP'
-  },
-  {
-    url: './fixtures/snmp.json',
-    name: 'Open SNMP'
-  },
-  {
-    url: './fixtures/ssdp.json',
-    name: 'Open SSDP'
-  }
-]
 
 // graphData (List of urls to API endpoints) should be passed from server
-
-const data = fixtures;
-
 ReactDOM.render(
-  <App urls={data}/>,
+  <App urls={graphData || []}/>,
   document.getElementById('root')
 );
