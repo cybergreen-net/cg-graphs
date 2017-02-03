c810a2a646adb664ad44ef7a0f3b9337dbf85814
zelima <ir.mchedlishvili@gmail.com>
2017-02-03 22:36:16 +0400

    [mockup][m]: mock up state and have it under tests refs #11

--- src/components/CountryPerformanceOnRisk.js
+++ src/components/CountryPerformanceOnRisk.js
@@ -6,46 +6,54 @@ class CountryPerformanceOnRisk extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: []
+      data: [],
+      graphOptions: {}
     }
   }
 
 
+  computeState() {
+    let xValues = ['2017-01-01','2017-01-08','2017-01-15'];
+    let state = {
+      data : [
+        {
+          x: xValues,
+          y: [2,4,6],
+          name: 'example N1',
+          type: 'scatter'
+        },
+        {
+          x: xValues,
+          y: [1,4,7],
+          name: 'example N2',
+          type: 'scatter'
+        }
+      ],
+      graphOptions: {
+        title : 'Global DDOS potential',
+        height: 600,
+        barmode: 'stack',
+        xaxis: {
+          title: '*This chart assumes an average 1 mbit/sec Internet connection for every IP address.',
+          gridcolor: 'transparent',
+        },
+        yaxis: {
+          title: 'GBit/sec'
+        }
+      }
+    }
+    return state
+  }
+
   componentDidMount() {
+    this.setState(this.computeState())
   };
 
 
   render() {
-    let xValues = ['2017-01-01','2017-01-08','2017-01-15'];
-    let trace = [
-      {
-        x: xValues,
-        y: [2,4,6],
-        name: 'example N1',
-        type: 'scatter'
-      },
-      {
-        x: xValues,
-        y: [1,4,7],
-        name: 'example N2',
-        type: 'scatter'
-      }
-    ];
-    let graphOptions = {
-      title : 'Global DDOS potential',
-      height: 600,
-      barmode: 'stack',
-      xaxis: {
-        title: '*This chart assumes an average 1 mbit/sec Internet connection for every IP address.',
-        gridcolor: 'transparent',
-      },
-      yaxis: {
-        title: 'GBit/sec'
-      }
-    }
     return (
       <div>
-        <PlotlyGraph data={trace} graphOptions={graphOptions} />
+        <PlotlyGraph data={this.state.data} graphOptions={this.state.graphOptions} />
       </div>
     );
   }
--- tests/CountryPerformanceOnRisk.component.test.js
+++ tests/CountryPerformanceOnRisk.component.test.js
@@ -5,7 +5,15 @@ import {shallow} from 'enzyme'
 
 describe('Components are working fine', () => {
   it('Div with id plot is there', () => {
-    const wrapper = shallow(< CountryPerformanceOnRisk urls={[]}/>)
-    expect(wrapper.html()).toEqual('<div><div id="plot"></div></div>')
+    const wrapper = shallow(< CountryPerformanceOnRisk />)
+    expect(wrapper.html()).toContain('<div><div id="plot"></div></div>')
+  })
+
+
+  it('computeState works', () => {
+    const wrapper = shallow(< CountryPerformanceOnRisk />)
+    let out = wrapper.instance().computeState();
+    expect(out.hasOwnProperty('data')).toBeTruthy();
+    expect(out.hasOwnProperty('graphOptions')).toBeTruthy();
   })
 })
