f49611e2760a514dfaa3bbd77c65d22db959b2fa
zelima <ir.mchedlishvili@gmail.com>
2017-02-04 18:07:12 +0400

    [#11][s]: simplify back only displaying one graph.

--- src/components/CountryPerformanceOnRisk.js
+++ src/components/CountryPerformanceOnRisk.js
@@ -19,33 +19,32 @@ class CountryPerformanceOnRisk extends Component {
   computeState() {
     let xValues = ['2017-01-01','2017-01-08','2017-01-15'];
     let reduxStore = {
+      graphs: {
+        1: {
+          title: 'DDOS-graph',
+          dataToshow: ['t1','t2'],
+          graphLayout: ['l1']
+        }
+      },
       entities: {
-        graphs: {
-            1: {
-            title: 'DDOS-graph',
-            dataToshow: ['t1','t2'],
-            graphLayout: ['l1']
-          }
-         },
-        data: {
-          t1: {
+        data: [
+          {
             x: xValues,
             y: [2,4,6],
             name: 'example N1',
             type: 'scatter'
           },
-          t2: {
+          {
             x: xValues,
             y: [1,4,7],
             name: 'example N2',
             type: 'scatter'
           }
-        },
+        ],
         layouts: {
           l1: {
             title : 'Global DDOS potential',
             height: 600,
-            barmode: 'stack',
             xaxis: {
               title: '*This chart assumes an average 1 mbit/sec Internet connection for every IP address.',
               gridcolor: 'transparent',
@@ -59,13 +58,10 @@ class CountryPerformanceOnRisk extends Component {
           {id: 'uk', name: 'United Kingdom'},
           {id: 'us', name: 'United States'}
         ]
-      },
-      graphsToShow: [1]
+      }
     }
 
     let state = {
-      graphsToShow: reduxStore.graphsToShow,
-      graphs: reduxStore.entities.graphs,
       data : reduxStore.entities.data,
       graphOptions: reduxStore.entities.layouts,
       countries: reduxStore.entities.countries,
@@ -93,31 +89,16 @@ class CountryPerformanceOnRisk extends Component {
 
 
   render() {
-    let graphsToShow = this.state.graphsToShow
-    let graphs = this.state.graphs
-    let data = this.state.data
     let graphOptions = this.state.graphOptions
     return (
       <div>
-        {graphsToShow.map(id => {
-          let dataForGraph = graphs[id].dataToshow.map(line => {
-            return data[line]
-          })
-          let layoutForGraph = graphs[id].graphLayout.map(layout => {
-            return graphOptions[layout]
-          })
-          return <div key={graphs[id].title}>
-            < PlotlyGraph
-            data={dataForGraph}
-            graphOptions={layoutForGraph}
-            graphID={graphs[id].title}/>
-
-            < input type="text"
-              placeholder="Search.."
-              value={this.state.search}
-              onChange={this.handleSearch.bind(this)} />
-          </div>
-        })}
+        <PlotlyGraph
+          data={this.state.data}
+          graphOptions={this.state.graphOptions}
+          graphID='DDOS-graph' />
+        <input type="text"
+          placeholder="Search.."
+          onChange={this.handleSearch.bind(this)} />
       </div>
     );
   }
