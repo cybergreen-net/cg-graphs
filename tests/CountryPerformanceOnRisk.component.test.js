3808a10ac14d93bcc4a95b13c2ecde3d67fe7441
zelima <ir.mchedlishvili@gmail.com>
2017-02-06 12:55:45 +0400

    [test][m]: added tests for checking if container state is updated after country is selected.

--- tests/CountryPerformanceOnRisk.component.test.js
+++ tests/CountryPerformanceOnRisk.component.test.js
@@ -1,6 +1,8 @@
-import {CountryPerformanceOnRisk, CountrySelect } from '../src/components/CountryPerformanceOnRisk.js'
-import React from 'react'
-import {shallow} from 'enzyme'
+import { CountryPerformanceOnRisk, CountrySelect }
+from '../src/components/CountryPerformanceOnRisk.js';
+import React from 'react';
+import { Provider } from 'react-redux';
+import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 
@@ -17,11 +19,52 @@ describe('Components are working fine', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   })
 
+  it('When a country is selected, it updates state of the container', () => {
+    function stub() {return ''}
+    const wrapper = shallow(< CountryPerformanceOnRisk dispatch={stub} />)
+    let newCountry = {value: 'us', label: 'United States'}
+    expect(wrapper.find('CountrySelect').at(2).props().selectedCountries)
+      .toEqual(undefined)
+    wrapper.find('CountrySelect').at(2).simulate('change', newCountry)
+    expect(wrapper.find('CountrySelect').at(2).props().selectedCountries)
+      .toEqual(newCountry)
+  })
+
 })
 
-describe('Country Select', () => {
+describe('Country Select component', () => {
+  function setup() {
+    let props = {
+      selectOptions: [
+        {value: '', label: 'Select a country'},
+        {value: 'uk', label: 'United Kingdom' }
+      ],
+      onChange: jest.fn(),
+      selectedCountries: undefined
+    }
+
+    const enzymeWrapper = shallow(< CountrySelect {...props} />)
+
+    return {
+      props,
+      enzymeWrapper
+    }
+  }
+
   it('Dropdown is there', () => {
     const wrapper = shallow(< CountrySelect selectOptions={[]}/>)
     expect(toJson(wrapper)).toMatchSnapshot();
+  })
+
+  it('Selecting a country triggers event on parent component', () => {
+    const { enzymeWrapper, props } = setup()
+    const selector = enzymeWrapper.find('Select')
+    expect(selector.props().value).toEqual({
+      "label": "Select a country", "value": ""
+    })
+    let selectedOption = {value: 'uk', label: 'United Kingdom' }
+    selector.props().onChange(selectedOption)
+    expect(props.onChange.mock.calls.length).toEqual(1)
+    expect(props.onChange.mock.calls[0][0]).toEqual(selectedOption)
   })
 })
