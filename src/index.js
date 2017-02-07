a8e9c603d06ceb0087274de0930d927d31c0d3c3
zelima <ir.mchedlishvili@gmail.com>
2017-02-07 04:33:58 +0400

    [store][l]: Sore refactore to use mockeddata for store that would recieve after actuall API call. - refs #18

--- src/index.js
+++ src/index.js
@@ -9,21 +9,14 @@ import CountryPerformanceOnRisk from './components/CountryPerformanceOnRisk';
 
 let xValues = ['2017-01-01','2017-01-08','2017-01-15'];
 let reduxStore = {
-  graphs: {
-    1: {
-      title: 'dns-graph',
-      dataToshow: ['uk1','t1'],
-      graphLayout: ['l1']
-    }
-  },
   entities: {
     countries: {
       '': {title: 'Select a country'},
-      'T': {title: 'Global'},
-      'GE': {title: 'Georgia'},
-      'KZ': {title: 'Kazakhstan'},
-      'UK': {title: 'United Kingdom'},
-      'US': {title: 'United States'}
+      't': {title: 'Global'},
+      'ge': {title: 'Georgia'},
+      'kz': {title: 'Kazakhstan'},
+      'gb': {title: 'United Kingdom'},
+      'us': {title: 'United States'}
     },
     risks: {
       1: {title: 'Open DNS'},
@@ -33,43 +26,80 @@ let reduxStore = {
       6: {title: 'Open Mirai'},
       100: {title: 'DDOS'}
     },
-    data: [
-      {
-        id: 'uk1',
-        x: xValues,
-        y: [2,4,6],
-        name: 'United Kingdom',
-        type: 'scatter'
-      },
-      {
-        id: 't1',
-        x: xValues,
-        y: [1,4,7],
-        name: 'Global',
-        type: 'scatter'
-      },
-      {
-        id: 'us1',
-        x: xValues,
-        y: [6,3,2],
-        name: 'United States',
-        type: 'scatter'
-      },
-      {
-        id: 'ge1',
-        x: xValues,
-        y: [5,12,1],
-        name: 'Georgia',
-        type: 'scatter'
-      },
-      {
-        id: 'kz1',
-        x: xValues,
-        y: [0,12,10],
-        name: 'Kazakstan',
-        type: 'scatter'
+    cubeByRiskByCountry: {
+      1: {
+        gb: [
+          {
+            "risk": 1,"country": "GB","date": "2017-01-16",
+            "count": "11506","count_amplified": 4571746
+          },
+          {
+            "risk": 1,"country": "GB","date": "2017-01-09",
+            "count": "13330","count_amplified": 4646530
+          },
+          {
+            "risk": 1,"country": "GB","date": "2017-01-02",
+            "count": "11471","count_amplified": 4570311
+          }
+        ],
+        ge: [
+          {
+            "risk": 1,"country": "GE","date": "2017-01-16",
+            "count": "25712","count_amplified": 234192
+          },
+          {
+            "risk": 1,"country": "GE","date": "2017-01-09",
+            "count": "15898","count_amplified": 241818
+          },
+          {
+            "risk": 1,"country": "GE","date": "2017-01-02",
+            "count": "6324","count_amplified": 259284
+          }
+        ],
+        kz: [
+          {
+            "risk": 1,"country": "KZ","date": "2017-01-16",
+            "count": "29399","count_amplified": 1205359
+          },
+          {
+            "risk": 1,"country": "KZ","date": "2017-01-09",
+            "count": "30580","count_amplified": 1253780
+          },
+          {
+            "risk": 1,"country": "KZ","date": "2017-01-02",
+            "count": "27083","count_amplified": 1110403
+          }
+        ],
+        us: [
+          {
+            "risk": 1,"country": "US","date": "2017-01-16",
+            "count": "82772","count_amplified": 35373652
+          },
+          {
+            "risk": 1,"country": "US","date": "2017-01-09",
+            "count": "56717","count_amplified": 35125397
+          },
+          {
+            "risk": 1,"country": "US","date": "2017-01-02",
+            "count": "66268","count_amplified": 35516988
+          },
+        ],
+        t: [
+          {
+            "risk": 1,"country": "T","date": "2017-01-16",
+            "count": "81548","count_amplified": 156434762
+          },
+          {
+            "risk": 1,"country": "T","date": "2017-01-09",
+            "count": "33172","count_amplified": 157100602
+          },
+          {
+            "risk": 1,"country": "T","date": "2017-01-02",
+            "count": "32558","count_amplified": 156849067
+          }
+        ]
       }
-    ],
+    },
     layouts: {
       l1: {
         title : 'Open DNS',
@@ -83,32 +113,11 @@ let reduxStore = {
       }
     }
   },
-  defaultCountry: {value: 'uk', label: 'United Kingdom' }
+  defaultCountry: {value: 'gb', label: 'United Kingdom' }
 }
 
 const reducer = (state, action) => {
-  // makes new copy of list, for not to mutate previous state
-  let newDataToShow = state.graphs[1].dataToshow
-  newDataToShow[action.idx] = action.id
-
-  switch(action.type) {
-    case 'addRemoveLine':
-      return {...state,
-        graphs: {
-          1: {
-            title: 'dns-graph',
-            dataToshow: Object.assign(
-              [],
-              state.graphs[1].dataToshow,
-              newDataToShow
-            ),
-            graphLayout: ['l1']
-          }
-        }
-      }
-    default:
-      return state
-  }
+  return state
 }
 
 let store = createStore(reducer, reduxStore)
