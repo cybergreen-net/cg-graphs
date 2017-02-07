cdc9f1dc96045b351f68a66d26da3ad86eb28eba
zelima <ir.mchedlishvili@gmail.com>
2017-02-07 23:18:47 +0400

    [reducers][m]: added reducer for select action. Fetchig reducers refactor to not mutate state

--- src/reducers/cubeReducers.js
+++ src/reducers/cubeReducers.js
@@ -1,43 +1,57 @@
+import update from 'react/lib/update'
 import {
-  FETCH_DATA_REQUEST, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE
+  FETCH_DATA_REQUEST, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, SELECT
 } from '../actions/cubeActions';
 
-export function buildCube(state, action) {
+const initialState = {
+  entities: {
+    countries: {},
+    risks: {},
+    cubeByRiskByCountry: {}
+  },
+  views: { 1: {} }
+}
+
+export function buildCube(state=initialState, action) {
   switch (action.type) {
     case FETCH_DATA_FAILURE:
-      return Object.assign({}, state, {
-        views: Object.assign({}, state.views, { 1: {
-                type: 'country/performance',
-                risk: action.risk, country: action.country,
-                isFetched: false, isFetching: false, didFailed: true,
-                errorMessage: action.errorMessage
-                }
-              }
-            )
+      return update(state, {
+        views: {
+          1 :{
+            type: {$set: 'country/performance'},
+            risk: {$set: action.risk}, country: {$set: action.country},
+            isFetched: {$set: false},
+            isFetching: {$set: false},
+            didFailed: {$set: true},
+            errorMessage: {$set: action.error}
           }
-        )
+        }
+      })
     case FETCH_DATA_REQUEST:
-      return Object.assign({}, state, {
-        views: Object.assign({}, state.views, { 1: {
-                type: 'country/performance',
-                risk: action.risk, country: action.country,
-                isFetched: false, isFetching: true, didFailed: false,
-                }
-              }
-            )
+      return update(state, {
+        views: {
+          1 :{
+            type: {$set: 'country/performance'},
+            risk: {$set: action.risk}, country: {$set: action.country},
+            isFetched: {$set: false},
+            isFetching: {$set: true},
+            didFailed: {$set: false}
           }
-        )
+        }
+      })
     case FETCH_DATA_SUCCESS:
-      return Object.assign({}, state, {
-          views: Object.assign({}, state.views, { 1: {
-                type: 'country/performance',
-                risk: action.risk, country: action.country,
-                isFetched: true, isFetching: false, didFailed: false,
-              }
-            }
-          )
-        },
-        {
+      let newState = update(state, {
+        views: {
+          1 :{
+            type: {$set: 'country/performance'},
+            risk: {$set: action.risk}, country: {$set: action.country},
+            isFetched: {$set: true},
+            isFetching: {$set: false},
+            didFailed: {$set: false}
+          }
+        }
+      })
+      return Object.assign({}, newState, {
           entities: Object.assign(
               {}, state.entities, {
                 cubeByRiskByCountry: Object.assign(
@@ -51,6 +65,17 @@ export function buildCube(state, action) {
           )
         }
       )
+    case SELECT:
+      return update(state, {
+        views: {
+          1: {
+            selectorConfig: {
+              $splice: [[action.idxOfSelector, 1,{
+                disabled: false, country: action.selectedCountry
+              }]]}
+          }
+        }
+      })
     default:
       return state
   }
