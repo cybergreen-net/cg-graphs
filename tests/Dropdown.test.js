123e82a5976917ed9701a7511318248321fa3a14
zelima <ir.mchedlishvili@gmail.com>
2017-02-09 17:31:50 +0400

    [test][m]: test for Dropdown component. checks snapshot and if state is changed when country is selected -refs 151 from stats-new

---
+++ tests/Dropdown.test.js
@@ -0,0 +1,38 @@
+import Dropdown from '../src/components/Dropdown';
+import React from 'react';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+describe('Dropdown component is working fine', () => {
+  function setup() {
+    let props = {
+      countries: {
+        't': {id: 't', name: 'Global', slug: ''},
+        'ge': {id: 'ge', name: 'Georgia', slug: 'georgia'},
+        'kz': {id: 'kz', name: 'Kazakhstan', slug: 'Kazakhstan'},
+        'gb': {id: 'gb', name: 'United Kingdom', slug: 'united-kingdom'},
+        'us': {id: 'us', name: 'United States', slug: 'united-states'}
+      }
+    }
+    const enzymeWrapper = shallow(< Dropdown {...props} />)
+
+    return {
+      props,
+      enzymeWrapper
+    }
+  }
+
+  it('Renders expected DOM', () => {
+    const { enzymeWrapper } = setup()
+    expect(toJson(enzymeWrapper)).toMatchSnapshot();
+  })
+
+  it('When a country is selected, it updates state of the component', () => {
+    const { enzymeWrapper, props } = setup()
+    let newCountry = {value: 'us', label: 'United States', slug: 'united-states'}
+    expect(enzymeWrapper.state().selectedCountry).toEqual(undefined)
+    enzymeWrapper.find('Select').simulate('change', newCountry)
+    expect(enzymeWrapper.state().selectedCountry).toEqual(newCountry)
+  })
+
+})
