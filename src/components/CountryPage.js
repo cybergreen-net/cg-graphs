179fc173048a171c97f9df923a863d833caf7386
anuveyatsu <anuar.ustayev@gmail.com>
2017-02-14 16:53:16 +0600

    [component][s]: refactored CountryPage to render AS bar chart, updated snapshot for it - refs #37

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryPerformanceOnRisk from './CountryPerformanceOnRisk';
+import { SourceOfInfection } from './SourceOfInfection';
 
 
 export class CountryPage extends Component {
@@ -11,8 +12,13 @@ export class CountryPage extends Component {
   render() {
     return (
       <div>
-        {Object.keys(this.props.views).map((key) => {
-          return <CountryPerformanceOnRisk view={this.props.views[key]} key={key} viewId={key}/>
+        {Object.keys(this.props.views).map((key, idx) => {
+          return (
+            <div key={key}>
+              <CountryPerformanceOnRisk view={this.props.views[key]} viewId={key}/>
+              <SourceOfInfection view={this.props.views[key]} viewId={key+idx} data={this.props.data}/>
+            </div>
+          )
         })}
       </div>
     );
@@ -20,9 +26,10 @@ export class CountryPage extends Component {
 }
 
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
   return {
-    views: state.countryPerformanceOnRiskViews
+    views: state.countryPerformanceOnRiskViews,
+    data: state.entities.cubeByRiskByAS
   }
 }
 
