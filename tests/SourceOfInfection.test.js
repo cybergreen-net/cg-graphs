90d7f38e8e1ff1e7dd0d6254c895c3f9cd3cdf7f
anuveyatsu <anuar.ustayev@gmail.com>
2017-02-15 20:14:40 +0600

    [tests][s]: updated tests and snapshots for SourceOfInfection component and CountryPage - refs #37

--- tests/SourceOfInfection.test.js
+++ tests/SourceOfInfection.test.js
@@ -6,6 +6,7 @@ import toJson from 'enzyme-to-json';
 describe('Component is working', () => {
   function setup() {
     let props = {
+      risks: {1: {title: 'Open DNS'}},
       view: {
         id: 'GB/1',
         country: 'GB',
@@ -17,40 +18,64 @@ describe('Component is working', () => {
           didFailed: false
         }
       },
-      data: [
-        {
-          country: "GB",
-          risk: 1,
-          date: "2017-01-01",
-          count: 2000,
-          as: [
+      data: {
+        1: {
+          GB: [
             {
-              id: 192,
-              count: 500
+              country: "GB",
+              risk: 1,
+              date: "2017-01-01",
+              granularity: "",
+              count: 2000,
+              count_normalized: 123,
+              count_amplified: "321",
+              as: [
+                {
+                  id: 192,
+                  count: 500,
+                  count_amplified: ""
+                },
+                {
+                  id: 254,
+                  count: 550,
+                  count_amplified: ""
+                },
+                {
+                  id: 214,
+                  count: 780,
+                  count_amplified: ""
+                }
+              ]
             },
             {
-              id: 254,
-              count: 550
-            }
-          ]
-        },
-        {
-          country: "GB",
-          risk: 1,
-          date: "2017-02-01",
-          count: 2000,
-          as: [
-            {
-              id: 192,
-              count: 440
-            },
-            {
-              id: 254,
-              count: 620
+              country: "GB",
+              risk: 1,
+              date: "2017-02-01",
+              granularity: "",
+              count: 2000,
+              count_normalized: 123,
+              count_amplified: "321",
+              as: [
+                {
+                  id: 192,
+                  count: 440,
+                  count_amplified: ""
+                },
+                {
+                  id: 254,
+                  count: 620,
+                  count_amplified: ""
+                },
+                {
+                  id: 214,
+                  count: 710,
+                  count_amplified: ""
+                }
+              ]
             }
           ]
         }
-      ]
+      }
     }
 
     const enzymeWrapper = shallow(<SourceOfInfection {...props} />)
@@ -77,13 +102,20 @@ describe('Component is working', () => {
 
   it('convertToPlotlySeries works', () => {
     const { enzymeWrapper, props } = setup()
-    let out = enzymeWrapper.instance().convertToPlotlySeries(props.data, props.view.AS.id[0])
+    let out = enzymeWrapper.instance().convertToPlotlySeries(
+      props.data[props.view.risk][props.view.country],
+      props.view.AS.id[0]
+    )
     expect(out.type).toEqual('bar')
     expect(out.x).toEqual(['2017-01-01', '2017-02-01'])
     expect(out.y).toEqual([500, 440])
     expect(out.name).toEqual(props.view.AS.id[0])
-    out = enzymeWrapper.instance().convertToPlotlySeries(props.data, undefined, true)
-    expect(out.y).toEqual([950, 940])
+    out = enzymeWrapper.instance().convertToPlotlySeries(
+      props.data[props.view.risk][props.view.country],
+      undefined,
+      true
+    )
+    expect(out.y).toEqual([170, 230])
     expect(out.name).toEqual('All the rest')
   })
 
