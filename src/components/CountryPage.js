c2c741e0d8ecc124a6ecee370a910d52b514ed1a
anuveyatsu <anuar.ustayev@gmail.com>
2017-02-20 16:43:03 +0600

    [component][m]: refactored CountryPage to have dropdown element with list of ASN passed from Redux store, also includes spinner, highlighter and redirection - refs #42

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -4,15 +4,79 @@ import CountryPerformanceOnRisk from './CountryPerformanceOnRisk';
 import SourceOfInfection from './SourceOfInfection';
 import '../css/temp.css' //this is temp import - needs to be removed for bundle
 
+import Highlighter from 'react-highlight-words'
+import Select from 'react-select';
+import Loader from 'halogen/BounceLoader'
+import 'react-select/dist/react-select.css';
+
 
 export class CountryPage extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      selectedASN: undefined,
+      inputValue: '',
+      spinner: '0px'
+    }
   }
 
+
+  onChange(asn) {
+    this.setState({
+      selectedASN: asn,
+      spinner: '20px'
+    });
+    if (asn.value) {
+      window.location = `/asn/${asn.value}`
+    }
+  }
+
+
+  optionRenderer(option) {
+    if(!option.label){ return }
+    return (
+      <Highlighter
+        searchWords={[this.state.inputValue]}
+        textToHighlight={option.label}
+      />
+    );
+  }
+
+
+  setInputValue(value) {
+    this.setState({
+      inputValue: value
+    })
+  }
+
+
   render() {
+    const selectOptions = Object.values(this.props.asn).map(asn => {
+      return {
+        value: asn.number,
+        label: asn.title
+      }
+    })
+    let spinnerStyle = { padding:'20px', margin: 'auto', width:'10%'}
+    selectOptions.unshift({value: '', label: 'Select an AS'})
     return (
       <div>
+        <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Select
+              name="asn-selector"
+              value={this.state.selectedASN || selectOptions[0]}
+              options={selectOptions}
+              onChange={this.onChange.bind(this)}
+              onInputChange={this.setInputValue.bind(this)}
+              optionRenderer={this.optionRenderer.bind(this)}
+            />
+            <div style={spinnerStyle}>
+              <Loader size={this.state.spinner} color='#00D49A'/>
+            </div>
+          </div>
+        </div>
         {Object.keys(this.props.views).map((key, idx) => {
           return (
             <div key={key} className="row">
@@ -30,7 +94,8 @@ export class CountryPage extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    views: state.countryPerformanceOnRiskViews
+    views: state.countryPerformanceOnRiskViews,
+    asn: state.entities.asn
   }
 }
 
