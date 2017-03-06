4cd4c43ffc7b244ce5a50234c58648a3ec6b63a3
anuveyatsu <anuar.ustayev@gmail.com>
2017-03-06 18:31:28 +0600

    [component][m]: computeState method for converting API response into plotly specific format - refs #73

--- src/components/ChoroplethMap.js
+++ src/components/ChoroplethMap.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import update from 'react/lib/update'
 import { connect } from 'react-redux';
 import PlotlyGraph from './Plot.js';
 import { fetchDataIfNeeded } from '../actions/ChoroplethMapActions';
@@ -28,6 +29,41 @@ export class ChoroplethMap extends Component {
     }
   }
 
+
+  computeState(props=this.props) {
+    let locations = []
+    let z = []
+    if(!props.view.isFetching && props.view.isFetched) {
+      props.data[props.view.risk][props.view.date].forEach(entry => {
+        if(props.countries[entry.country]) {
+          locations.push(props.countries[entry.country].name)
+          z.push(entry.count)
+        }
+      })
+
+      return update(this.state, {
+        data: [{
+          locations: {$set: locations},
+          z: {$set: z},
+          text: {$set: z}
+        }]
+      })
+    }
+
+    return {}
+  }
+
+
+  componentDidMount() {
+    this.props.dispatch(fetchDataIfNeeded(this.props.view.risk, this.props.view.date))
+  }
+
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(this.computeState(nextProps))
+  }
+
+
   render() {
     return (
       <div>
@@ -39,3 +75,15 @@ export class ChoroplethMap extends Component {
     )
   }
 }
+
+
+const mapStateToProps = (state) => {
+  return {
+    view: state.ChoroplethMapViews,
+    data: state.entities.cubeByRiskByDate,
+    countries: state.entities.countries
+  }
+}
+
+
+export default connect(mapStateToProps)(ChoroplethMap);
--- tests/ChoroplethMap.test.js
+++ tests/ChoroplethMap.test.js
@@ -9,3 +9,54 @@ describe('ChoroplethMap component', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   })
 })
+
+describe('custom methods in ChoroplethMap component', () => {
+  function setup() {
+    let props = {
+      dispatch: jest.fn(),
+      data: {
+        100: {
+          '2017-01-01': [
+            {country: 'GB', count: 123},
+            {country: 'US', count: 324}
+          ]
+        }
+      },
+      countries: {
+        'GB': {id: 'GB', name: 'United Kingdom', slug: 'united-kingdom'},
+        'US': {id: 'US', name: 'United States', slug: 'united-states'}
+      },
+      view: {
+        isFetched: false,
+        isFetching: false,
+        didFailed: false,
+        risk: 100,
+        date: '2017-01-01'
+      }
+    }
+
+    const enzymeWrapper = shallow(<ChoroplethMap {...props} />)
+
+    return {
+      props,
+      enzymeWrapper
+    }
+  }
+
+  it('checks if computeState method works correct', () => {
+    const { props, enzymeWrapper } = setup()
+    let out = enzymeWrapper.instance().computeState(props)
+    expect(out).toEqual({}) // because isFetched=false
+    props.view.isFetched = true
+    out = enzymeWrapper.instance().computeState(props)
+    expect(out.data[0].locations).toEqual([
+      props.countries['GB'].name,
+      props.countries['US'].name
+    ])
+    expect(out.data[0].z).toEqual([
+      props.data[100]['2017-01-01'][0].count,
+      props.data[100]['2017-01-01'][1].count
+    ])
+  })
+
+})
