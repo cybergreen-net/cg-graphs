e36eeb0a9b51e14984decfa23a840eff4a2750d2
anuveyatsu <anuar.ustayev@gmail.com>
2017-03-07 13:55:38 +0600

    [component][l]: refactored ChoroplethMap to render form with date and risk options; to have handleChange and handleSubmit methods; to render map with brand colours; added tests and updated snapshot - refs #73

--- src/components/ChoroplethMap.js
+++ src/components/ChoroplethMap.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import update from 'react/lib/update'
 import { connect } from 'react-redux';
 import PlotlyGraph from './Plot.js';
-import { fetchDataIfNeeded } from '../actions/ChoroplethMapActions';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import { fetchDataIfNeeded, riskAndDateAreSelected } from '../actions/ChoroplethMapActions';
 
 
 export class ChoroplethMap extends Component {
@@ -15,18 +17,59 @@ export class ChoroplethMap extends Component {
         locationmode: 'country names',
         locations: [],
         z: [],
-        text: [],
-        autocolorscale: true
+        autocolorscale: false,
+        colorbar: {
+          thickness: 10,
+          x: 0.8
+        },
+        zmin: 0,
+        zmax: 8200000,
+        colorscale: [
+          [0, 'rgb(231, 251, 245)'],
+          [0.2, 'rgb(162, 239, 218)'],
+          [0.4, 'rgb(115, 231, 199)'],
+          [0.6, 'rgb(46, 219, 172)'],
+          [0.8, 'rgb(0, 212, 154)'],
+          [1, 'rgb(0, 174, 127)']
+        ]
       }],
       layout: {
+        height: 500,
         title: 'Choropleth Map',
         geo: {
+          showcountries: true,
           projection: {
-              type: 'robinson'
+              type: 'equirectangular'
           }
         }
-      }
+      },
+      dateToShow: this.props.view.date,
+      riskToShow: this.props.view.risk
     }
+
+    this.handleChangeRisk = this.handleChange.bind(this, 'riskToShow')
+    this.handleChangeDate = this.handleChange.bind(this, 'dateToShow')
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+
+  handleChange(idx, object) {
+    this.setState({
+      [idx]: object.value
+    })
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.dispatch(fetchDataIfNeeded(
+      this.state.riskToShow,
+      this.state.dateToShow
+    ))
+    this.props.dispatch(riskAndDateAreSelected(
+      this.state.riskToShow,
+      this.state.dateToShow
+    ))
   }
 
 
@@ -44,8 +87,7 @@ export class ChoroplethMap extends Component {
       return update(this.state, {
         data: [{
           locations: {$set: locations},
-          z: {$set: z},
-          text: {$set: z}
+          z: {$set: z}
         }]
       })
     }
@@ -65,12 +107,41 @@ export class ChoroplethMap extends Component {
 
 
   render() {
+    const riskSelectOptions = Object.values(this.props.risks).map(risk => {
+      return {
+        value: risk.id,
+        label: risk.title
+      }
+    })
     return (
       <div>
         <PlotlyGraph
           data={this.state.data}
           graphOptions={this.state.layout}
           graphID={this.state.graphID}/>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="col-sm-2 col-sm-offset-4">
+              <Select
+                value={this.state.dateToShow}
+                name='dateToShow'
+                onChange={this.handleChangeDate}
+                clearable={false}
+                options={[{value: this.state.dateToShow, label: this.state.dateToShow}]}/>
+            </div>
+            <div className="col-sm-2">
+              <Select
+                value={this.state.riskToShow}
+                name='riskToShow'
+                onChange={this.handleChangeRisk}
+                clearable={false}
+                options={riskSelectOptions}/>
+            </div>
+            <div className="col-sm-1">
+              <input type="submit" value="Submit" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
@@ -81,7 +152,8 @@ const mapStateToProps = (state) => {
   return {
     view: state.ChoroplethMapViews,
     data: state.entities.cubeByRiskByDate,
-    countries: state.entities.countries
+    countries: state.entities.countries,
+    risks: state.entities.risks
   }
 }
 
--- tests/ChoroplethMap.test.js
+++ tests/ChoroplethMap.test.js
@@ -3,45 +3,50 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
 
+function setup() {
+  let props = {
+    dispatch: jest.fn(),
+    data: {
+      100: {
+        '2017-01-01': [
+          {country: 'GB', count: 123},
+          {country: 'US', count: 324}
+        ]
+      }
+    },
+    countries: {
+      'GB': {id: 'GB', name: 'United Kingdom', slug: 'united-kingdom'},
+      'US': {id: 'US', name: 'United States', slug: 'united-states'}
+    },
+    risks: {
+      1: {title: 'Open DNS', id: 1},
+      2: {title: 'Open NTP', id: 2}
+    },
+    view: {
+      isFetched: false,
+      isFetching: false,
+      didFailed: false,
+      risk: 100,
+      date: '2017-01-01'
+    }
+  }
+
+  const enzymeWrapper = shallow(<ChoroplethMap {...props} />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
 describe('ChoroplethMap component', () => {
   it('render PlotlyGraph component with expected props', () => {
-    const wrapper = shallow(<ChoroplethMap />)
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const { enzymeWrapper } = setup()
+    expect(toJson(enzymeWrapper)).toMatchSnapshot();
   })
 })
 
 describe('custom methods in ChoroplethMap component', () => {
-  function setup() {
-    let props = {
-      dispatch: jest.fn(),
-      data: {
-        100: {
-          '2017-01-01': [
-            {country: 'GB', count: 123},
-            {country: 'US', count: 324}
-          ]
-        }
-      },
-      countries: {
-        'GB': {id: 'GB', name: 'United Kingdom', slug: 'united-kingdom'},
-        'US': {id: 'US', name: 'United States', slug: 'united-states'}
-      },
-      view: {
-        isFetched: false,
-        isFetching: false,
-        didFailed: false,
-        risk: 100,
-        date: '2017-01-01'
-      }
-    }
-
-    const enzymeWrapper = shallow(<ChoroplethMap {...props} />)
-
-    return {
-      props,
-      enzymeWrapper
-    }
-  }
 
   it('checks if computeState method works correct', () => {
     const { props, enzymeWrapper } = setup()
@@ -57,6 +62,13 @@ describe('custom methods in ChoroplethMap component', () => {
       props.data[100]['2017-01-01'][0].count,
       props.data[100]['2017-01-01'][1].count
     ])
+  })
+
+  it('checks how handleChange method works', () => {
+    const { enzymeWrapper } = setup()
+    expect(enzymeWrapper.state().riskToShow).toEqual(100)
+    enzymeWrapper.instance().handleChange('riskToShow', {value: 1, label: 'DNS'})
+    expect(enzymeWrapper.state().riskToShow).toEqual(1)
   })
 
 })
